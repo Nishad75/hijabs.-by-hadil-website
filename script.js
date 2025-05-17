d23e7368075c668cd190f526d79f6b56593434a4
mohammad nishad <mohammadnishad1243.mn@gmail.com>
2025-05-17 20:47:03 +0530

    first step taken

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example: Increment cart count
+let cartCount = 0;
+
+function addToCart() {
+  cartCount++;
+  document.getElementById("cart-count").textContent = cartCount;
+}
+
+// Call this function when an item is added
+// addToCart();
